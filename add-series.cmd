db13b33643d571334c49e005657efc160ca75851
Manuel Grundner <m.grundner@delegate.at>
2019-05-17 10:26:45 +0200

    feat: added series description

--- add-series.cmd
+++ add-series.cmd
@@ -1,8 +1,8 @@
 mkdir series\%1
 
-echo --- >> series/%1/index.html
-echo layout: seriespage >> series/%1/index.html
-echo seriesid: %1 >> series/%1/index.html
-echo isseries: true >> series/%1/index.html
-echo title: %2 >> series/%1/index.html
-echo --- >> series/%1/index.html
+echo --- >> series/%1/index.md
+echo layout: seriespage >> series/%1/index.md
+echo seriesid: %1 >> series/%1/index.md
+echo isseries: true >> series/%1/index.md
+echo title: %2 >> series/%1/index.md
+echo --- >> series/%1/index.md
---
+++ series/t-is-for-testing-xaf-xpo/index.md
@@ -0,0 +1,10 @@
+---
+layout: seriespage
+seriesid: t-is-for-testing-xaf-xpo
+isseries: true
+title: "T is for Testing: XAF & XPO"
+---
+
+Testing is an important part of a developers responsibilities, especially in a fast moving agile world!  
+  
+This series focuses testing patterns I used and discovered when testing applications using XAF and XPO applications with different teams I worked with.
