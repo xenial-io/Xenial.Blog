f976fa5663fec199e26e41169801336456483e3b
Manuel Grundner <m.grundner@delegate.at>
2020-09-27 10:11:21 +0200

    feat: import styles from xenial-template

---
+++ js/index.ts
@@ -0,0 +1,5 @@
+import "../css/main.scss";
+
+import { xenial } from "@xenial-io/xenial-template";
+
+xenial();
---
+++ rollup.config.js
@@ -0,0 +1,83 @@
+import pkg from "./package.json";
+
+import { brotliCompressSync } from "zlib";
+
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import scss from "rollup-plugin-scss";
+import copy from "rollup-plugin-copy";
+import { terser } from "rollup-plugin-terser";
+import gzipPlugin from "rollup-plugin-gzip";
+import filesize from "rollup-plugin-filesize";
+import css from "rollup-plugin-css-porter";
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const additionalFiles = [
+  // "./_site/css/bundle.css",
+  // ...fg.sync("./_site/css/*.svg"),
+  // ...fg.sync("./_site/css/*.ttf"),
+  // ...fg.sync("./_site/img/*.svg"),
+  // ...fg.sync("./_site/img/*.png"),
+  // ...fg.sync("./_site/img/*.ico"),
+];
+
+export default [
+  {
+    input: "js/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "iife",
+        // plugins: [terser()]
+      },
+    ],
+    external: [],
+    plugins: [
+      resolve({ extensions }),
+      commonjs(),
+      babel({
+        extensions,
+        exclude: "node_modules/**",
+      }),
+      scss({
+        output: "_site/css/bundle.css",
+        // outputStyle: "compressed",
+      }),
+      copy({
+        targets: [
+          {
+            src: "node_modules/@xenial-io/ src/css/*.woff",
+            dest: "./_site/css",
+          },
+          {
+            src: "node_modules/@xenial-io/xenial-template/dist/css/*.woff2",
+            dest: "./_site/css",
+          },
+          {
+            src: "node_modules/@xenial-io/xenial-template/dist/css/*.ttf",
+            dest: "./_site/css",
+          },
+          {
+            src: "node_modules/@xenial-io/xenial-template/dist/css/*.svg",
+            dest: "./_site/css",
+          },
+          {
+            src: "node_modules/@xenial-io/xenial-template/dist/img/*.*",
+            dest: "./_site/img",
+          },
+        ],
+      }),
+      // gzipPlugin({
+      //   additionalFiles,
+      // }),
+      // gzipPlugin({
+      //   additionalFiles,
+      //   customCompression: (content) =>
+      //     brotliCompressSync(Buffer.from(content)),
+      //   fileName: ".br",
+      // }),
+      filesize(),
+    ],
+  },
+];
