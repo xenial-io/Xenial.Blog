5c0085cdadf3bea512addef4fda5e53757524c9f
Manuel Grundner <m.grundner@delegate.at>
2020-09-28 13:32:49 +0200

    feat: img lightbox

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,14 @@
 import "../css/main.scss";
+import * as basicLightbox from 'basiclightbox';
 
 import { xenial } from "@xenial-io/xenial-template";
 
 xenial();
+
+document.querySelectorAll("article img").forEach(el => {
+    const img = <HTMLImageElement>el;
+    img.onclick = ()  =>
+    {
+        basicLightbox.create(`<img src="${img.src}" />`).show();
+    };
+});
