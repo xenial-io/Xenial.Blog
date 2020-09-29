9f9d417c25d086cdcbdfd69d3c3e5b5e2cfa53f7
Manuel Grundner <m.grundner@delegate.at>
2020-09-29 12:48:40 +0200

    fix: tags to lower

--- add-tag.cmd
+++ add-tag.cmd
@@ -1,7 +1,7 @@
-mkdir tags\%1
+mkdir src\content\tags\%1
 
-echo --- >> tags/%1/index.html
-echo layout: tagpage >> tags/%1/index.html
-echo tag: %1 >> tags/%1/index.html
-echo title: "%1" >> tags/%1/index.html
-echo --- >> tags/%1/index.html
+echo --- >> src/content/tags/%1/index.html
+echo layout: tagpage >> src/content/tags/%1/index.html
+echo tag: %1 >> src/content/tags/%1/index.html
+echo title: "%1" >> src/content/tags/%1/index.html
+echo --- >> src/content/tags/%1/index.html
