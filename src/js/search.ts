1a88a4e8eb8574fcd9c913c2713f5b4960b2aa77
Manuel Grundner <m.grundner@delegate.at>
2020-09-29 16:58:58 +0200

    feat: compact header and search debounce

--- src/js/search.ts
+++ src/js/search.ts
@@ -1,22 +1,54 @@
+const debounce = (func: () => void, wait: number, immediate: boolean) => {
+  var timeout;
+
+  return function executedFunction() {
+    var context = this;
+    var args = arguments;
+
+    var later = function () {
+      timeout = null;
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    };
+
+    var callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+
+    if (callNow) {
+      func.apply(context, args);
+    }
+  };
+};
+
 const search = (inputClass: string, targetUl: string) => {
   const input = <HTMLInputElement>document.querySelector(inputClass);
   const ul = document.querySelector(targetUl);
   if (input && ul) {
     input.onkeyup = () => {
       const filter = input.value.toUpperCase();
-      ul.querySelectorAll("li").forEach((el) => {
-        el.classList.remove("visible");
-        el.classList.remove("hidden");
+      debounce(
+        () => {
+          ul.querySelectorAll("li").forEach((el) => {
+            el.classList.remove("visible");
+            el.classList.remove("hidden");
 
-        el.querySelectorAll("a").forEach((a) => {
-          const txtValue = a.textContent || a.innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            el.classList.add("visible");
-          } else {
-            el.classList.add("hidden");
-          }
-        });
-      });
+            el.querySelectorAll("a").forEach((a) => {
+              const txtValue = a.textContent || a.innerText;
+              if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                el.classList.add("visible");
+              } else {
+                el.classList.add("hidden");
+              }
+            });
+          });
+        },
+        200,
+        false
+      )();
     };
   }
 };
