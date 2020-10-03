592fb0130d0b804c1a69131f76895e168e27392b
Manuel Grundner <m.grundner@delegate.at>
2020-10-03 06:27:12 +0200

    feat: added bullseye build

---
+++ build/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using static Bullseye.Targets;
+
+using static SimpleExec.Command;
+
+namespace build
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            Target("npm install", () => RunAsync("npm", "install", windowsName: NpmLocation));
+            Target("npm run build", DependsOn("npm install"), () => RunAsync("npm", "run build", windowsName: NpmLocation));
+            Target("npm", DependsOn("npm run build"));
+
+            Target("build", DependsOn("npm"));
+
+            Target("default", DependsOn("build"));
+
+            return RunTargetsAndExitAsync(args);
+        }
+
+        static string NpmLocation => $@"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)}\nodejs\npm.cmd";
+    }
+}
