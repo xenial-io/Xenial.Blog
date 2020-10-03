f27d355cb466c5a59343d5027c1f3ae2dce3038f
Manuel Grundner <m.grundner@delegate.at>
2020-10-03 07:12:47 +0200

    build: clean and npm

--- build/Program.cs
+++ build/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 
 using static Bullseye.Targets;
@@ -11,11 +12,19 @@ namespace build
     {
         static Task Main(string[] args)
         {
-            Target("npm install", () => RunAsync("npm", "install", windowsName: NpmLocation));
-            Target("npm run build", DependsOn("npm install"), () => RunAsync("npm", "run build", windowsName: NpmLocation));
-            Target("npm", DependsOn("npm run build"));
+            Console.WriteLine($"{Directory.GetCurrentDirectory()}");
 
-            Target("build", DependsOn("npm"));
+            Target("noop", () => { });
+
+            Target("clean:npm", () => Ignored(() => RunAsync("cmd.exe", "/C rmdir /S /Q node_modules")));
+            Target("clean:_site", () => Ignored(() => RunAsync("cmd.exe", "/C rmdir /S /Q _site")));
+            Target("clean", DependsOn("clean:npm", "clean:_site"));
+
+            Target("npm:install", () => RunAsync("npm", "install", windowsName: NpmLocation));
+            Target("npm:run:build", DependsOn("npm:install"), () => RunAsync("npm", "run build", windowsName: NpmLocation));
+            Target("npm", DependsOn("npm:run:build"));
+
+            Target("build", DependsOn("clean", "npm"));
 
             Target("default", DependsOn("build"));
 
@@ -23,5 +32,31 @@ namespace build
         }
 
         static string NpmLocation => $@"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)}\nodejs\npm.cmd";
+
+        static async Task RunToolAsync(Func<Task> action)
+        {
+            try
+            {
+                await action();
+            }
+            catch (SimpleExec.NonZeroExitCodeException)
+            {
+                Console.WriteLine("Tool seams missing. Try to restore");
+                await RunAsync("dotnet", "tool restore");
+                await action();
+            }
+        }
+
+        static async Task Ignored(Func<Task> action)
+        {
+            try
+            {
+                await action();
+            }
+            catch (SimpleExec.NonZeroExitCodeException e)
+            {
+                Console.WriteLine($"Action Failed. Ignored {e}");
+            }
+        }
     }
 }
