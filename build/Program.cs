a4f14acdfa24feaea5202d880ff12ef10a781d36
Manuel Grundner <m.grundner@delegate.at>
2020-10-03 13:01:54 +0200

    feat: git version and branch

--- build/Program.cs
+++ build/Program.cs
@@ -3,12 +3,23 @@ using System.Threading.Tasks;
 using YamlDotNet.Serialization;
 
 using static Bullseye.Targets;
-
 using static SimpleExec.Command;
+using static System.Console;
+
+var version = new Lazy<Task<string>>(async () => await ReadToolAsync(() => ReadAsync("dotnet", "minver -v e")));
+var branch = new Lazy<Task<string>>(async () => await ReadAsync("git", "rev-parse --abbrev-ref HEAD"));
+var lastUpdate = new Lazy<Task<string>>(async () => await ReadAsync("git", "log -1 --format=%cd "));
 
 var NpmLocation = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)}\nodejs\npm.cmd";
 var blogDirectory = "src\\content";
 var defaultArguments = $"--s={blogDirectory} --destination=../../_site";
+
+Target("version", async () =>
+{
+    WriteLine(await version.Value);
+    WriteLine(await branch.Value);
+    WriteLine(await lastUpdate.Value);
+});
 
 Target("clean:npm", () => Ignored(() => RunAsync("cmd.exe", "/C rmdir /S /Q node_modules")));
 Target("clean:_site", () => Ignored(() => RunAsync("cmd.exe", "/C rmdir /S /Q _site")));
@@ -41,6 +52,21 @@ static async Task RunToolAsync(Func<Task> action)
         await action();
     }
 }
+
+static async Task<string> ReadToolAsync(Func<Task<string>> action)
+{
+    try
+    {
+        return await action();
+    }
+    catch (SimpleExec.NonZeroExitCodeException)
+    {
+        Console.WriteLine("Tool seams missing. Try to restore");
+        await RunAsync("dotnet", "tool restore");
+        return await action();
+    }
+}
+
 
 static async Task Ignored(Func<Task> action)
 {
