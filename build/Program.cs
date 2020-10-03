16e502ff60c7d2a9c72f3279f2d6897e088bbae6
Manuel Grundner <m.grundner@delegate.at>
2020-10-03 13:28:49 +0200

    feat: rev parse hash

--- build/Program.cs
+++ build/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using YamlDotNet.Serialization;
 
@@ -6,21 +8,34 @@ using static Bullseye.Targets;
 using static SimpleExec.Command;
 using static System.Console;
 
-var version = new Lazy<Task<string>>(async () => await ReadToolAsync(() => ReadAsync("dotnet", "minver -v e")));
-var branch = new Lazy<Task<string>>(async () => await ReadAsync("git", "rev-parse --abbrev-ref HEAD"));
-var lastUpdate = new Lazy<Task<string>>(async () => await ReadAsync("git", "log -1 --format=%cd "));
+var version = new Lazy<Task<string>>(async () => await ReadToolAsync(() => ReadAsync("dotnet", "minver -v e", noEcho: true)));
+var branch = new Lazy<Task<string>>(async () => await ReadAsync("git", "rev-parse --abbrev-ref HEAD", noEcho: true));
+var lastUpdate = new Lazy<Task<string>>(async () => await ReadAsync("git", "log -1 --format=%cd", noEcho: true));
+var hash = new Lazy<Task<string>>(async () => await ReadAsync("git", "rev-parse HEAD", noEcho: true));
 
 var NpmLocation = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)}\nodejs\npm.cmd";
 var pretzelLocation = @"C:\f\git\pretzel\src\Pretzel\bin\Release\net5\Pretzel.exe";
 var blogDirectory = "src\\content";
+var configFile = Path.Combine(blogDirectory, "_config.yml");
 var defaultArguments = $"-s={blogDirectory} -d=../../_site";
 
-Target("version", async () =>
+Target("version:read", async () =>
 {
-    WriteLine(await version.Value);
-    WriteLine(await branch.Value);
-    WriteLine(await lastUpdate.Value);
+    WriteLine($"Version: {await version.Value}");
+    WriteLine($"Branch: {await branch.Value}");
+    WriteLine($"Update: {await lastUpdate.Value}");
+    WriteLine($"Hash: {await hash.Value}");
 });
+
+Target("version:write", DependsOn("version:read"), async () =>
+{
+    var ymlContent = await File.ReadAllTextAsync(configFile);
+    var deserializer = new DeserializerBuilder().Build();
+    var result = deserializer.Deserialize<Dictionary<object, object>>(ymlContent);
+    Console.WriteLine(result["version"]);
+});
+
+Target("version", DependsOn("version:read", "version:write"));
 
 Target("clean:npm", () => Ignored(() => RunAsync("cmd.exe", "/C rmdir /S /Q node_modules")));
 Target("clean:_site", () => Ignored(() => RunAsync("cmd.exe", "/C rmdir /S /Q _site")));
