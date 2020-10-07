5a0aafdb538d97ba44b7999c6c1bcb6d8faba9ef
Manuel Grundner <m.grundner@delegate.at>
2020-10-07 17:36:49 +0200

    build: copy all the 💩

--- build/Program.cs
+++ build/Program.cs
@@ -120,11 +120,11 @@ Target("build:blog", DependsOn("version", "comments"), () => RunToolAsync(() => 
 
 Target("build", DependsOn("clean", "npm", "build:blog"));
 
-Target("deploy", async () =>
+var deployDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+Target("deploy:copy", async () =>
 {
     var connectionString = $"ftp://{Environment.GetEnvironmentVariable("FTP_USER")}:{Environment.GetEnvironmentVariable("FTP_PASS")}@{Environment.GetEnvironmentVariable("FTP_HOST")}";
-    
-    Console.WriteLine($"ConnectionString: {connectionString}");
 
     var config = new
     {
@@ -136,9 +136,12 @@ Target("deploy", async () =>
 
     await File.WriteAllTextAsync("_site/.creep.env", JsonConvert.SerializeObject(config, Formatting.Indented));
 
-    Console.WriteLine(await File.ReadAllTextAsync("_site/.creep.env"));
+    DirectoryCopy("_site", deployDirectory, true);
+});
 
-    await RunAsync("creep", "-b _site -y -v");
+Target("deploy", DependsOn("deploy:copy"), async () =>
+{
+    await RunAsync("creep", "-y -v", windowsName: "creep.exe", workingDirectory: deployDirectory);
 });
 
 Target("default", DependsOn("build"));
@@ -242,6 +245,41 @@ static IEnumerable<Comment> Flatten(Page page)
     }
 }
 
+static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+{
+    // Get the subdirectories for the specified directory.
+    var dir = new DirectoryInfo(sourceDirName);
+
+    if (!dir.Exists)
+    {
+        throw new DirectoryNotFoundException(
+            "Source directory does not exist or could not be found: "
+            + sourceDirName);
+    }
+
+    var dirs = dir.GetDirectories();
+
+    // If the destination directory doesn't exist, create it.       
+    Directory.CreateDirectory(destDirName);
+
+    // Get the files in the directory and copy them to the new location.
+    var files = dir.GetFiles();
+    foreach (var file in files)
+    {
+        var tempPath = Path.Combine(destDirName, file.Name);
+        file.CopyTo(tempPath, false);
+    }
+
+    // If copying subdirectories, copy them and their contents to new location.
+    if (copySubDirs)
+    {
+        foreach (var subdir in dirs)
+        {
+            var tempPath = Path.Combine(destDirName, subdir.Name);
+            DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+        }
+    }
+}
 
 public class Page
 {
