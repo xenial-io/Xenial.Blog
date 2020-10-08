848e14af151e1ad3d7f7f1bdbd9e68cc31d0f41b
Manuel Grundner <m.grundner@delegate.at>
2020-10-08 09:53:43 +0200

    feat: added easter egg

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,13 +4,11 @@ import { search } from "./search";
 import { comments } from "./comments";
 import { xenial } from "@xenial-io/xenial-template";
 
-
 xenial();
 
 document.querySelectorAll("article .postcontent img").forEach(el => {
     const img = <HTMLImageElement>el;
-    img.onclick = ()  =>
-    {
+    img.onclick = () => {
         basicLightbox.create(`<img src="${img.src}" />`).show();
     };
 });
@@ -19,3 +17,10 @@ search(".tag-overview__search-input", ".tag-overview__list");
 search(".archive__search-input", ".archive__list");
 search(".series__search-input", ".series__list");
 comments();
+
+
+document.querySelectorAll(".egg").forEach(el => {
+    (el as HTMLElement).onclick = async () => {
+        window.open("https://unsplash.com/s/photos/austria", "_blank");
+    };
+});
